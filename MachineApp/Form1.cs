ffcb7fab54755b68f97b54269fdd19f9fd31662d
Chelsey LaPointe <lapchek@dunwoody.edu>
2022-09-13 11:35:36 -0500

    Added inventory page

---
+++ MachineApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineShopInventoryPage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student_Intvy_Page();
+        }
+
+        public void Student_Intvy_Page()
+        {
+            btnAdd.Hide();
+            btnEdit.Hide();
+            btnRemove.Hide();
+            btnSignout.Hide();
+            btnSignin.Show();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            btnSignin.Hide();
+            btnSignout.Show();
+            btnAdd.Show();
+            btnEdit.Show();
+            btnRemove.Show();
+        }
+
+        private void btnSignout_Click(object sender, EventArgs e)
+        {
+            Student_Intvy_Page();
+        }
+    }
+}
---
+++ MachineApp/Form1.designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace MachineShopInventoryPage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InventoryList = new System.Windows.Forms.ListBox();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbCategorize = new System.Windows.Forms.ComboBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSignout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory";
+            // 
+            // InventoryList
+            // 
+            this.InventoryList.FormattingEnabled = true;
+            this.InventoryList.ItemHeight = 16;
+            this.InventoryList.Location = new System.Drawing.Point(66, 123);
+            this.InventoryList.Name = "InventoryList";
+            this.InventoryList.Size = new System.Drawing.Size(953, 292);
+            this.InventoryList.TabIndex = 1;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Location = new System.Drawing.Point(66, 439);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(107, 34);
+            this.btnSignin.TabIndex = 2;
+            this.btnSignin.Text = "Sign in";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            this.btnSignin.Click += new System.EventHandler(this.btnSignin_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(751, 43);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(107, 34);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // cbCategorize
+            // 
+            this.cbCategorize.FormattingEnabled = true;
+            this.cbCategorize.Items.AddRange(new object[] {
+            "Tool Name",
+            "Category"});
+            this.cbCategorize.Location = new System.Drawing.Point(886, 49);
+            this.cbCategorize.Name = "cbCategorize";
+            this.cbCategorize.Size = new System.Drawing.Size(133, 24);
+            this.cbCategorize.TabIndex = 4;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(912, 439);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(107, 34);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(613, 439);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(107, 34);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(361, 439);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(107, 34);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(491, 439);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(107, 34);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnSignout
+            // 
+            this.btnSignout.Location = new System.Drawing.Point(66, 439);
+            this.btnSignout.Name = "btnSignout";
+            this.btnSignout.Size = new System.Drawing.Size(107, 34);
+            this.btnSignout.TabIndex = 9;
+            this.btnSignout.Text = "Sign out";
+            this.btnSignout.UseVisualStyleBackColor = true;
+            this.btnSignout.Click += new System.EventHandler(this.btnSignout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 512);
+            this.Controls.Add(this.btnSignout);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.cbCategorize);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.InventoryList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox InventoryList;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbCategorize;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSignout;
+    }
+}
+
