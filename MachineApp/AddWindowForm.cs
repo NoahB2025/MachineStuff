f497c07c5a3f9ff8175a9b8900590cbbbf94fd7f
Chelsey LaPointe <lapchek@dunwoody.edu>
2022-09-13 12:11:36 -0500

    AddWindowForm

---
+++ MachineApp/AddWindowForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineApp
+{
+    public partial class AddWindowForm : MetroFramework.Forms.MetroForm
+    {
+        public AddWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddWindowForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
